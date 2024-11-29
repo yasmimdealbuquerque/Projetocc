951b5350987095f6d9ddcb5b01fafb964f050b7d
jLucyJhon <jhonkirigaya2005@gmail.com>
2024-11-29 11:00:56 -0300

    commit
    Até o presente momento é oq temos

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
-#include <iostream> 
-#include <conio.h> 
-#include <windows.h> 
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
 #include <locale.h>
-//incluir biblioteca pra gravar as musicas - acho que é a fstream 
+// incluir biblioteca pra gravar as musicas - acho que é a fstream
 
 using namespace std;
 
-void playNotes(int freq, int tempo){
-    if (freq > 0){
+void playNotes(int freq, int tempo)
+{
+    if (freq > 0)
+    {
         Beep(freq, tempo);
     }
 }
 
-void guitarraVirtual(){
-   while (true){
-    char notes = _getch();
+void guitarraVirtual()
+{
+    while (true)
+    {
+        char notes = _getch();
 
-    switch (notes){
+        switch (notes)
+        {
         case 'q':
             playNotes(82, 350);
             break;
@@ -37,28 +42,31 @@ void guitarraVirtual(){
             break;
         case 'b':
             cout << "Não quer mais tocar guitarra ?" << "\n"
-                 << "Bom, você não é um Jimmy Page, mas continue tentando ^-^"<< endl;
+                 << "Bom, você não é um Jimmy Page, mas continue tentando ^-^" << endl;
+                 
+
             return;
             system("pause");
         default:
             cout << "Bem, parece que teclou errado... -_-" << endl;
-            break; 
-            		
-
+            break;
+        }
     }
-   }
 }
 
-void bateriaVirtual(){
-    while(true){
+void bateriaVirtual()
+{
+    while (true)
+    {
         char notes = _getch();
 
-            switch (notes){
+        switch (notes)
+        {
         case '7':
-            playNotes(200, 400); // caixa 
+            playNotes(200, 400); // caixa
             break;
         case '8':
-            playNotes(115, 400); // tom grave 
+            playNotes(115, 400); // tom grave
             break;
         case '9':
             playNotes(180, 400); // tom medio
@@ -76,60 +84,65 @@ void bateriaVirtual(){
             playNotes(5500, 400); // prato
             break;
         case '0':
-            cout << "Éhhh... o Eloy Casagrande ainda é melhor, mas tudo bem °l°"<< endl;
+            cout << "Éhhh... o Eloy Casagrande ainda é melhor, mas tudo bem °l°" << endl;
             return;
         default:
             cout << "Bem, parece que é melhor você tentar mais tarde... ^-^'" << endl;
-            break; 
+            break;
         }
     }
-        
 }
 
-int main (){
+int main()
+{
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
-	SetConsoleOutputCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
 
     system("color BD");
 
     cout << " " << endl;
     cout << "Aoba, esse é nosso 'joguinho' musical, seja bem vindo!" << endl;
-    cout << "Temos dois instrumentos diferentes pra tocar"<< endl;
+    cout << "Temos dois instrumentos diferentes pra tocar" << endl;
     cout << " " << endl;
     cout << "Faça sua escolha" << endl;
     cout << "1 - Guitarra" << endl;
     cout << "2 - Bateria" << endl;
     cout << "3 - Sair do joguinho" << endl;
-    
+
     char escolha;
     cin >> escolha;
 
-    if (escolha == '1'){
+    if (escolha == '1')
+    {
         cout << "Pode tocar sua guitarra " << endl;
         cout << "Teclas: de q a y" << endl;
         cout << "Encerrar: b" << endl;
+
         guitarraVirtual();
         system("pause");
         system("cls");
-
-    }else if(escolha == '2'){
-        cout <<"Pode tocar sua bateria"<< endl;
+    }
+    else if (escolha == '2')
+    {
+        cout << "Pode tocar sua bateria" << endl;
         cout << "Teclas: de 7 a 1" << endl;
         cout << "Encerrar: 0" << endl;
         bateriaVirtual();
         system("pause");
         system("cls");
-    }else if (escolha == '3'){
+    }
+    else if (escolha == '3')
+    {
         cout << "Obrigada por tocar, não esqueça de voltar pra praticar L(^^)" << endl;
         system("pause");
         system("cls");
-    }else {
-        cout <<"Escolhe algo que existe cara, pfvr..."<< endl;
+    }
+    else
+    {
+        cout << "Escolhe algo que existe cara, pfvr..." << endl;
         system("pause");
-        
     }
     system("pause");
-    return 1; 
+    return 1;
 }
-
