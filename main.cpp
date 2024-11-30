84be05ee1532bb627e8c3c64904fe4202b8b48c4
yasmim <164027041+yasmimdealbuquerque@users.noreply.github.com>
2024-11-29 22:19:51 -0300

    estrutura de funções of e if streams adicionadas, mas precisando de ajustes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <windows.h> 
 #include <locale.h>
 #include <fstream> 
+#include <string>
 
 using namespace std;
 
@@ -12,32 +13,39 @@ void playNotes(int freq, int tempo){
     }
 }
 
-void guitarraVirtual(){
+void guitarraVirtual(string musicDados){
    while (true){
     char notes = _getch();
 
     switch (notes){
         case 'q':
             playNotes(82, 350);
+            musicDados += "82 350\n";
             break;
         case 'w':
             playNotes(110, 350);
+            musicDados += "110 350\n";
             break;
         case 'e':
             playNotes(146, 350);
+            musicDados += "146 350\n";
             break;
         case 'r':
             playNotes(196, 350);
+            musicDados += "196 350\n";
             break;
         case 't':
             playNotes(246, 350);
+            musicDados += "246 350\n";
             break;
         case 'y':
             playNotes(329, 350);
+            musicDados += "329 350\n";
             break;
         case 'b':
             cout << "Não quer mais tocar guitarra ?" << "\n"
-                 << "Bom, você não é um Jimmy Page, mas continue tentando ^-^"<< endl;
+                 << "Bom, você não é um Jimmy Page, mas continue tentando ^-^"<< "\n"
+                 << "Sua música está sendo gravada, aguarde '-' " << endl;
             return;
             system("pause");
         default:
@@ -48,52 +56,88 @@ void guitarraVirtual(){
    }
 }
 
-void bateriaVirtual(){
+void bateriaVirtual(string musicDados){
     while(true){
         char notes = _getch();
 
             switch (notes){
         case '7':
-            playNotes(200, 400); // caixa 
+            playNotes(200, 400); // caixa
+            musicDados += "200 350\n"; 
             break;
         case '8':
-            playNotes(115, 400); // tom grave 
+            playNotes(115, 400); // tom grave
+            musicDados += "115 350\n";
             break;
         case '9':
             playNotes(180, 400); // tom medio
+            musicDados += "180 350\n";
             break;
         case '4':
             playNotes(280, 400); // tom agudo
+            musicDados += "280 350\n";
             break;
         case '5':
             playNotes(80, 400); // bumbo
+            musicDados += "80 350\n";
             break;
         case '6':
             playNotes(5000, 400); // hi-hat
+            musicDados += "5000 350\n";
             break;
         case '2':
             playNotes(5500, 400); // prato
+            musicDados += "5500 350\n";
             break;
         case '0':
-            cout << "Éhhh... o Eloy Casagrande ainda é melhor, mas tudo bem °l°"<< endl;
+            cout << "Éhhh... o Eloy Casagrande ainda é melhor, mas tudo bem °l°"<< "\n"
+                << "Estamos terminando de gravar seu som, um segundo :}" << endl;
             return;
         default:
             cout << "Bem, parece que é melhor você tentar mais tarde... ^-^'" << endl;
             break; 
         }
     }
-        
+    
+}
+
+void saveMusic(const string nameFile, const string musicDados){
+    ofstream saveArquivo(nameFile, ofstream::out);
+    if (saveArquivo.is_open()){
+        saveArquivo << "Notas: " << "\n" 
+                    << musicDados << endl; //era pra escrever o valor que recebe dentro do switch
+        cout << "Música salva, hehe :D" << endl; //adicionar algnus cls 
+    }else{
+        cout <<"Vish, deu erro ao salvar ¨_¨ "<<endl;
+    }
+    saveArquivo.close();
+}
+
+void playMusic(const string nameFile){
+    ifstream musicFile(nameFile);
+    if (musicFile.is_open()){
+        cout <<"Abrindo o arquivo com sua música..." << endl;
+    }else{
+        cout << "Não conseguimos abrir o arquivo com sua música :(" << endl;
+        return;
+    }
+
+    int freq, tempo;
+    while (musicFile >> freq >> tempo){
+        playNotes(freq, tempo);
+    }
+    musicFile.close();
+    cout << "Olhaaa, que música hein, já podes ir sangrar os ouvidos das pessoas no rock in rio "<< endl;
+    
 }
 
 int main (){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 	SetConsoleOutputCP(CP_UTF8);
-
     system("color BD");
 
-    string const(nomeMusica);
-    string nomeFile;
+    
     char escolha;
     do { // é pra rodar o bloco baixo várias vezes, mas ele vai testar antes 
         cout << " " << endl;
@@ -103,44 +147,51 @@ int main (){
         cout << "Faça sua escolha e aperte 'enter' " << endl;
         cout << "1 - Guitarra" << endl;
         cout << "2 - Bateria" << endl;
-        cout << "3 - Sair do joguinho" << endl;
+        cout << "3 - Reproduzir música" << endl;
+        cout << "4 - Sair do joguinho" << endl;
 
         cin >> escolha;
+        string nameMusic;
 
-        if (escolha == '1'){
-            cout << "Pode tocar sua guitarra " << endl;
-            cout << "Teclas: de q a y" << endl;
-            cout << "Encerrar: b" << endl;
-            guitarraVirtual();
-            
-            system("pause");
-            system("cls");
+        if (escolha == '1' || escolha == '2'){
+            string nameMusic;
+            cout << "Qual o nome da sua música ? (digite sem espaços): ";
+            cin >> nameMusic;
 
-            // socorro Deeeeeussssss
-            cout << "Dê um nome pra música que você tocou: " << endl;
-            cin >> nomeMusica;
+            string nameFile = nameMusic + ".txt";
+            string musicDados = "";
+       
 
-            nomeFile = nomeMusica + ".txt";
-            
-            ofstream file (nomeMusica, ofstream::out);
+            if (escolha == '1'){
+                cout << "Pode tocar sua guitarra " << endl;
+                cout << "Teclas: de q a y" << endl;
+                cout << "Encerrar: b" << endl;
+                guitarraVirtual(musicDados);
+                
+                system("pause");
+                system("cls");
 
-            file << "Música: " 	<< nomeMusica	<< "\n"
-                 << "Frequências: " <<  Beep << "\n";
-            
-            file.close();
+            }else if(escolha == '2'){
+                cout <<"Pode tocar sua bateria"<< endl;
+                cout << "Teclas: de 7 a 1" << endl;
+                cout << "Encerrar: 0" << endl;
+                bateriaVirtual(musicDados);
+                
+                system("pause");
+                system("cls");
+        }
 
-            system("pause");
-            system("cls");
-            //
+        saveMusic(nameFile, musicDados);
 
-        }else if(escolha == '2'){
-            cout <<"Pode tocar sua bateria"<< endl;
-            cout << "Teclas: de 7 a 1" << endl;
-            cout << "Encerrar: 0" << endl;
-            bateriaVirtual();
-            system("pause");
-            system("cls");
         }else if (escolha == '3'){
+           string nameMusic;
+           cout << "Qual música você quer reproduzir?: ";
+           cin >> nameMusic;
+           
+           string nameFile = nameMusic + ".txt"; // provavelmente vou colocar dentro da função
+           playMusic(nameFile);
+
+        }else if (escolha == '4'){
             cout << "Obrigada por tocar, não esqueça de voltar pra praticar L(^^)" << endl;
             system("pause");
             system("cls");
@@ -149,7 +200,7 @@ int main (){
             system("pause");   
             system("cls"); 
         }
-    }while(escolha != '3');
+    }while(escolha != '4');
     system("pause");
     return 0; 
 }
